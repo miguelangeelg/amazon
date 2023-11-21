e6c388ed09fecbe94e199153b72027bacc9d6cef
Miguel <miguelangelofl@hotmail.com>
2023-11-21 23:16:58 +0000

    second commit

--- dynamodb.py
+++ dynamodb.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import boto3
+
+print(boto3)
+
+#create a function to list tables of dynamodb
+def list_tables():
+    dynamodb = boto3.resource('dynamodb')
+    for table in dynamodb.tables.all():
+        print(table.name)
+        
+
+#create a function to show tables of prueba table
+def show_table():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('prueba')
+    for item in table.scan()['Items']:
+        print(item) 
+
+show_table()
+
