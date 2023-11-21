3416fe26dad845ef8f22e8f7a73c0c0c2d021183
Miguel <miguelangelofl@hotmail.com>
2023-11-21 23:49:33 +0000

    Creating menu and adding option to create a table

--- dynamodb.py
+++ dynamodb.py
@@ -16,5 +16,41 @@ def show_table():
     for item in table.scan()['Items']:
         print(item) 
 
-show_table()
+# create function to create a table in dynamodb with capacity under demand
+def create_table(name):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.create_table(
+        TableName=name,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        BillingMode='PAY_PER_REQUEST'
+    )
+    print("Tabla creada exitosamente")
 
+
+option = 1
+while option != 0:
+    print("Ingrese la opcion que desea")
+    print("1. Listar tablas de la cuenta")
+    print("2. Crear una tabla")
+    option = int(input())
+    if option == 1:
+        list_tables()
+    elif option == 2:
+        print("Ingrese nombre para la tabla")
+        name = str(input())
+        create_table(name)
+    elif option == 0:
+        print("Hasta luego")
+    else:
+        print("Opcion invalida")
